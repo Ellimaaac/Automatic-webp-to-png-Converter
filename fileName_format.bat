0fae4dc26c046d137ff9d0217171c7cd9879b92e
Camille <144267933+Ellimaaac@users.noreply.github.com>
2024-09-14 07:50:29 +0200

    Update fileName_format.bat

--- fileName_format.bat
+++ fileName_format.bat
@@ -1,7 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-
-:: This script replaces spaces with underscores in file names in the current directory
 for %%f in (* *) do (
   set "oldname=%%f"
   set "newname=!oldname: =_!"
